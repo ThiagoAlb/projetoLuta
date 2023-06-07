e5b164f566140e87402934cff38f7629abdbbc65
Thiago Albuquerque <95172551+ThiagoAlb@users.noreply.github.com>
2023-06-07 11:02:13 -0300

    Finalização do projeto usando POO

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
+let log = new Log(document.querySelector('.log'));
+
 let char = new Knight('Thiago');
-let monster = new littleMonster();
+let monster = new bigMonster();
 
 const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 );
 
 stage.start();
